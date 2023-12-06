7519d0b8b91135a769a88e9f81cf8f8580e94e5a
Antonin Haas <tondahaas@gmail.com>
2023-12-06 15:38:01 +0100

    day 6 refactored

--- day06/part1.py
+++ day06/part1.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from functools import reduce
 
 with open('input.txt') as f:
     lines = f.read().splitlines()
@@ -6,14 +7,6 @@ with open('input.txt') as f:
 parsed = [x.split(": ")[1].split() for x in lines]
 time = [eval(i) for i in parsed[0]]
 distance = [eval(i) for i in parsed[1]]
-init = 1
-
-for t, d in zip(time, distance):
-    records = 0
-    for i in range(t+1):
-        run = i * (t-i)
-        if run > d:
-            records += 1
-    init *= records
-
-print(init)
+records = [sum(1 for i in range(t + 1) if i * (t - i) > d) for t, d in zip(time, distance)]
+result = reduce((lambda x, y: x * y), records)
+print(result)

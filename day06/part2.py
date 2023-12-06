b3c97e0d2437161a108e02d2a139cea78b15b3ad
Antonin Haas <tondahaas@gmail.com>
2023-12-06 15:38:20 +0100

    day 6 refactored

--- day06/part2.py
+++ day06/part2.py
@@ -4,13 +4,7 @@ with open('input.txt') as f:
     lines = f.read().splitlines()
 
 parsed = [x.split(": ")[1].replace(" ", "") for x in lines]
-time = eval(parsed[0])
-distance = eval(parsed[1])
-records = 0
-
-for i in range(time+1):
-    run = i * (time-i)
-    if run > distance:
-        records += 1
-
-print(records)
+time = int(parsed[0])
+distance = int(parsed[1])
+result = sum(1 for i in range(time + 1) if i * (time - i) > distance)
+print(result)
